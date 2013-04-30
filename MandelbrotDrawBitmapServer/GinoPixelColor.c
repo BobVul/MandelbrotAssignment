0959468795479a91a8d13bd285957acea4978336
GinoConte <ginoconte95@gmail.com>
2013-04-30 19:34:49 +1000

    Update GinoPixelColor.c

--- MandelbrotDrawBitmapServer/GinoPixelColor.c
+++ MandelbrotDrawBitmapServer/GinoPixelColor.c
@@ -1,70 +1,52 @@
 #define MAX_ITERATIONS 256
 
 
-unsigned char stepsToBlue (int steps) {
-    if (steps > 200 && steps < 256 ) {
-      return 0xFF * steps / MAX_ITERATIONS * 2;
-    } else if (steps >= MAX_ITERATIONS) {
-    	return 0x22;
-    } else {
-    	return 0xaa;
-    }
-}
 unsigned char stepsToRed (int steps) {
-if (steps < 20) {
-	return 0x00;
-}
-return 0xcc;
+
+	int valueRed;
+	if (steps < 20) {
+		valueRed = 0x00;
+	} else {
+		valueRed = 0xcc;
+	}
+	if (steps % 3 == 0 ) {
+    	valueRed = 0xff;
+    }
+	
+	return valueRed;
 }
 unsigned char stepsToGreen (int steps) {
 
+	int valueGreen;
     if (steps > 200 && steps < 256 ) {
-    	return 0xFF * steps / MAX_ITERATIONS * 2;
+    	valueGreen = 0xFF * steps / MAX_ITERATIONS * 2;
     } else if (steps >= MAX_ITERATIONS) {
-    	return 0x55;
+    	valueGreen = 0x55;
     } else if (steps < 100) {
-    	return 0xFF * steps / 30;
+    	valueGreen = 0xFF * steps / 30;
     } else {
-    	return 0xff;
+    	valueGreen = 0xff;
     }
-    	
-    	
+    if (steps % 3 == 0 ) {
+    	valueGreen = 0xff * 23 * steps / MAX_ITERATIONS;
+    }
+    
+    return valueGreen;   	
 }
 
-/*unsigned char stepsToRed (int steps) {
-    if (steps < 200) {
-    	return 0xFF * steps / MAX_ITERATIONS * 4;
-    } else {
-    	return 0xFF * steps / MAX_ITERATIONS;
-    }
-}
 unsigned char stepsToBlue (int steps) {
-    if (steps > 200) {
-    	return 0xFF * steps / MAX_ITERATIONS * 0;
-    } else if (steps < 100) {
-    	return 0xAA * steps / MAX_ITERATIONS;
+
+	int valueBlue;
+    if (steps > 200 && steps < 256 ) {
+    	valueBlue = 0xFF * steps / MAX_ITERATIONS * 2;
+    } else if (steps >= MAX_ITERATIONS) {
+    	valueBlue = 0x22;
     } else {
-    	return 0x00;
+    	valueBlue = 0xaa;
     }
-}
-unsigned char stepsToGreen (int steps) {
-    if (steps < 200) {
-    	return 0xFF * steps / MAX_ITERATIONS * 1/3;
-    } else {
-    	return 0xFF * 4 * steps / MAX_ITERATIONS;
+    if (steps % 3 == 0 ) {
+    	valueBlue = 0x00;
     }
-    	
-}*/
-
-
-/*#define MAX_ITERATIONS 256
-
-unsigned char stepsToRed (int steps) {
-    return (0xFF * 1/2 * steps / MAX_ITERATIONS);
+    
+    return valueBlue;
 }
-unsigned char stepsToBlue (int steps) {
-    return (0xFF * 3 * steps / MAX_ITERATIONS);
-}
-unsigned char stepsToGreen (int steps) {
-    return (0xFF * 1/4 * steps / MAX_ITERATIONS);
-}*/
