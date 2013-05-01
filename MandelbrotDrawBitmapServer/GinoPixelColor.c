24f9cf7278d9cb0b88fde453481030c786f9680c
GinoConte <ginoconte95@gmail.com>
2013-05-01 16:05:12 +1000

    Update GinoPixelColor.c

--- MandelbrotDrawBitmapServer/GinoPixelColor.c
+++ MandelbrotDrawBitmapServer/GinoPixelColor.c
@@ -50,3 +50,56 @@ unsigned char stepsToBlue (int steps) {
     
     return valueBlue;
 }
+
+/*#define MAX_ITERATIONS 256
+
+
+unsigned char stepsToRed (int steps) {
+
+	int valueRed;
+	if (steps < 20) {
+		valueRed = 0x00;
+	} else {
+		valueRed = 0xcc;
+	}
+	if (steps % 3 == 0 ) {
+    	valueRed = 0xff;
+    }
+	
+	return valueRed;
+}
+unsigned char stepsToGreen (int steps) {
+
+	int valueGreen;
+    if (steps > 200 && steps < 256 ) {
+    	valueGreen = 0xFF * steps / MAX_ITERATIONS * 2;
+    } else if (steps >= MAX_ITERATIONS) {
+    	valueGreen = 0x55;
+    } else if (steps < 100) {
+    	valueGreen = 0xFF * steps / 30;
+    } else {
+    	valueGreen = 0xff;
+    }
+    if (steps % 3 == 0 ) {
+    	valueGreen = 0xaa;
+    }
+    
+    return valueGreen;   	
+}
+
+unsigned char stepsToBlue (int steps) {
+
+	int valueBlue;
+    if (steps > 200 && steps < 256 ) {
+    	valueBlue = 0xFF * steps / MAX_ITERATIONS * 2;
+    } else if (steps >= MAX_ITERATIONS) {
+    	valueBlue = 0x22;
+    } else {
+    	valueBlue = 0xaa;
+    }
+    if (steps % 3 == 0 ) {
+    	valueBlue = 0x00;
+    }
+    
+    return valueBlue;
+} */
